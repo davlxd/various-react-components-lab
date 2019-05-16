ad303d046901b6c14a4ad366afe0e6d6246efa4a
lxd <i@lxd.me>
2019-05-16 21:41:04 +1200

    Timeline framework done

--- src/components/Timeline/d3/axis-in-d3.js
+++ src/components/Timeline/d3/axis-in-d3.js
@@ -23,6 +23,7 @@ const drawAxis = (svg, width, yearSeries) => {
   const ARROW_HEAD_HEIGHT = 8
   const AXIS_WIDTH = 4
   const SCALE_MARKER_WIDTH = 15
+
   svg.append('defs')
        .append('marker')
          .attr('id', 'arrow-head')
@@ -87,19 +88,43 @@ const drawDescBackground = (svg, axisRightBoundary, width, yearSeries, ranges) =
   const PER_MOMTH_HEIGHT = PER_YEAR_HEIGHT / 12
 
   const g = svg.append('g').attr('class', 'desc')
+
+  const eachLinearGradient = svg.select('defs')
+                                .selectAll('linearGradient.desc-rect')
+                                .data(ranges)
+                                .enter()
+                                  .append('linearGradient')
+                                  .attr('class', 'desc-rect')
+                                  .attr('id', (d, i) => `lineargradient-desc-rect-${i}`)
+
+  eachLinearGradient.append('stop')
+                    .attr('offset', '10%')
+                    .attr('stop-color', (d, i) => d3.schemePastel1[i])
+                    .attr('stop-opacity', 0)
+  eachLinearGradient.append('stop')
+                    .attr('offset', '70%')
+                    .attr('stop-color', (d, i) => d3.schemePastel1[i])
+                    .attr('stop-opacity', 0.6)
+  eachLinearGradient.append('stop')
+                    .attr('offset', '100%')
+                    .attr('stop-color', (d, i) => d3.schemePastel1[i])
+                    .attr('stop-opacity', 0.6)
+
+
   const eachDesc = g.selectAll('rect.range')
                     .data(rangesToMonthDiff(ranges, maxYear))
                     .enter()
                       .append('g')
                       .attr('class', 'range')
 
+  const COLOR_PADDING_RIGHT = 10
   eachDesc.append('rect')
-          .attr('class', 'desc-background')
+          .attr('id', (d, i) => `desc-rect-${i}`)
           .attr('x', axisRightBoundary)
           .attr('y', d => maxYearY + d.monthDiffWithMaxYear * PER_MOMTH_HEIGHT)
-          .attr('width', width - axisRightBoundary)
+          .attr('width', width - axisRightBoundary - COLOR_PADDING_RIGHT)
           .attr('height', d => d.monthDiffWithEachOther * PER_MOMTH_HEIGHT)
-          .style('fill', '#ffffff')
+          .style('fill',  (d, i) => `url(#lineargradient-desc-rect-${i})`)
   eachDesc.append('line')
           .attr('class', 'ceiling')
           .attr('x1', axisRightBoundary)
--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 
+import marked from 'marked';
+
 import { initateSvg, drawAxis, drawDescBackground } from './d3/axis-in-d3'
 const moment = require('moment')
 
 const DEFAULT_WIDTH = 800
 const styles = theme => ({
   root: {
-    backgroundColor: '#f3f9fe',
+    // backgroundColor: '#f3f9fe',
+  },
+  descDiv: {
+    position: 'absolute',
+    padding: 20,
+    paddingLeft: 50,
   }
 })
 
@@ -17,6 +24,10 @@ class Timeline extends Component {
     super(props)
     this.divId = 'timeline-svg-div'
     this.svgId = 'timeline-svg'
+    this.state = {
+      descRectLocations: [
+      ]
+    }
   }
 
   extractYearSeries(ranges) {
@@ -31,6 +42,18 @@ class Timeline extends Component {
     return yearSeries
   }
 
+  positionDescDiv(ranges) {
+    this.setState({
+      descRectLocations: ranges.map((_, index) => document.getElementById(`desc-rect-${index}`).getBoundingClientRect())
+                               .map(({ top, left, height, width }) => ({
+                                  top: top + window.pageYOffset || document.documentElement.scrollTop,
+                                  left: left + window.pageXOffset || document.documentElement.scrollLeft,
+                                  height,
+                                  width
+                                }))
+    })
+  }
+
   componentDidMount() {
     const { divId, svgId } = this
     const { ranges } = this.props.data
@@ -39,15 +62,25 @@ class Timeline extends Component {
     const svg = initateSvg(divId, svgId, DEFAULT_WIDTH, yearSeries.length)
     const axisRightBoundary = drawAxis(svg, DEFAULT_WIDTH, yearSeries)
     drawDescBackground(svg, axisRightBoundary, DEFAULT_WIDTH, yearSeries, ranges)
+
+    this.positionDescDiv(ranges)
+    window.onresize = () => this.positionDescDiv(ranges)
   }
 
   render() {
     const { classes } = this.props
+    const { ranges } = this.props.data
+    const { descRectLocations } = this.state
     const { divId, svgId } = this
 
     return (
       <div id={divId} className={classes.root}>
         <svg id={svgId}></svg>
+        {ranges.map((range, index) => (
+          <div key={index} className={classes.descDiv} id={`desc-div-${index}`}
+               style={{  ...descRectLocations[index]  }}
+               dangerouslySetInnerHTML={{ __html: marked(range.desc)}} />
+        ))}
       </div>
     )
   }
