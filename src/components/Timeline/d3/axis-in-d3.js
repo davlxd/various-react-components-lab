5aace38a0e89a4c7502e5c1d51799eeb90c469f2
lxd <i@lxd.me>
2019-05-16 20:05:00 +1200

    Reforat axis

--- src/components/Timeline/d3/axis-in-d3.js
+++ src/components/Timeline/d3/axis-in-d3.js
@@ -18,27 +18,32 @@ const initateSvg = (divId, svgId, width, howManyYears) => {
 }
 
 const drawAxis = (svg, yearSeries, width) => {
-  const MARGIN = 50
+  const ARROW_HEAD_WIDTH = 8
+  const ARROW_HEAD_HEIGHT = 8
+  const AXIS_WIDTH = 4
+  const SCALE_MARKER_WIDTH = 15
   svg.append('defs')
        .append('marker')
          .attr('id', 'arrow-head')
-         .attr('markerWidth', 10)
-         .attr('markerHeight', 8)
-         .attr('refX', 5)
-         .attr('refY', 4)
+         .attr('markerWidth', ARROW_HEAD_WIDTH)
+         .attr('markerHeight', ARROW_HEAD_HEIGHT)
+         .attr('refX', ARROW_HEAD_WIDTH / 2)
+         .attr('refY', ARROW_HEAD_HEIGHT / 2)
            .append('path')
-             .attr('d', 'M1,8 L5,0 L9,8 L5,5 Z')
+             .attr('d', 'M0,8 L4,0 L8,8 L4,5 Z')
+
   const g = svg.append('g')
                  .attr('class', 'axis')
-  g.append('line')
-     .attr('class', 'axis')
-     .attr('x1', MARGIN)
-     .attr('y1', height(yearSeries.length) - MARGIN)
-     .attr('x2', MARGIN)
-     .attr('y2', MARGIN)
-     .attr('stroke', 'black')
-     .attr('stroke-width', 4)
-     .attr('marker-end', 'url(#arrow-head)')
+
+  const axisLine = g.append('line')
+                    .attr('class', 'axis')
+                    .attr('x1', AXIS_WIDTH / 2)
+                    .attr('y1', height(yearSeries.length))
+                    .attr('x2', AXIS_WIDTH / 2)
+                    .attr('y2', ARROW_HEAD_HEIGHT * 2)
+                    .attr('stroke', 'black')
+                    .attr('stroke-width', AXIS_WIDTH)
+                    .attr('marker-end', 'url(#arrow-head)')
 
   const eachScale = g.selectAll('line.scale')
                      .data(yearSeries.reverse())
@@ -46,21 +51,25 @@ const drawAxis = (svg, yearSeries, width) => {
                        .append('g')
                        .attr('class', 'scale')
 
-  eachScale.append('line')
-          .attr('x1', MARGIN - 10)
-          .attr('y1', (d, i) => MARGIN + (i + 0.5) * PER_YEAR_HEIGHT)
-          .attr('x2', MARGIN + 10)
-          .attr('y2', (d, i) => MARGIN + (i + 0.5) * PER_YEAR_HEIGHT)
-          .attr('stroke-width', 1)
-          .attr('stroke', 'black')
+  const eachScaleMarker = eachScale.append('line')
+                                   .attr('x1', 0)
+                                   .attr('y1', (d, i) => (i + 1) * PER_YEAR_HEIGHT)
+                                   .attr('x2', SCALE_MARKER_WIDTH)
+                                   .attr('y2', (d, i) => (i + 1) * PER_YEAR_HEIGHT)
+                                   .attr('stroke-width', 1)
+                                   .attr('stroke', 'black')
 
   const eachScaleText = eachScale.append('text')
                                  .attr('x', 0)
-                                 .attr('y', (d, i) => MARGIN + (i + 0.5) * PER_YEAR_HEIGHT)
+                                 .attr('y', (d, i) => (i + 1) * PER_YEAR_HEIGHT)
                                  .text(d => d)
   const scaleTextBBox = index => eachScaleText.nodes()[index].getBBox()
 
   eachScaleText.attr('dy', (d, i) => scaleTextBBox(i).height / 4)
+  axisLine.attr('x1', AXIS_WIDTH / 2 + scaleTextBBox(0).width + SCALE_MARKER_WIDTH / 2)
+          .attr('x2', AXIS_WIDTH / 2 + scaleTextBBox(0).width + SCALE_MARKER_WIDTH / 2)
+  eachScaleMarker.attr('x1', AXIS_WIDTH / 2 + scaleTextBBox(0).width)
+                 .attr('x2', AXIS_WIDTH / 2 + scaleTextBBox(0).width + SCALE_MARKER_WIDTH)
 }
 
 
